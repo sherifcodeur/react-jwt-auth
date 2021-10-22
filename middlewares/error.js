a5dc1379602aa0e368ae410094cb08c47659b2a1
sherifcodeur <sherifcodeur@gmail.com>
2021-10-22 20:11:08 +0100

    created error utils

---
+++ middlewares/error.js
@@ -0,0 +1,12 @@
+const ErrorResponse = require('../utils/errorResponse')
+
+
+
+const errorHandler = (err,req,res,next)=>{
+
+
+    let error = {...err}
+
+
+
+}
---
+++ utils/errorResponse.js
@@ -0,0 +1,16 @@
+
+
+class ErrorResponse extends Error {
+
+    constructor(message,statusCode){
+
+        super(message)
+
+        this.statusCode = statusCode
+    }
+
+
+}
+
+
+module.exports = ErrorResponse;
